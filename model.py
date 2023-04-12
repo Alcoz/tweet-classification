81000eeffa295116020d9a9e695d08c8121ea2be
Baptiste Darnala <baptiste.darnala@elzeard.co>
2023-04-12 22:44:47 +0200

    requirements

--- model.py
+++ model.py
@@ -23,9 +23,9 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 import re
-#nltk.download('averaged_perceptron_tagger')
-#nltk.download('stopwords')
-#nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 tk = TweetTokenizer()
 lemmatizer = WordNetLemmatizer()
@@ -77,5 +77,5 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 def vec(tweet):
     vectorized_tweet = vectorizer.transform([tweet])
     return torch.Tensor(vectorized_tweet.toarray()).to(torch.float64).to(device)
-    
+
     
